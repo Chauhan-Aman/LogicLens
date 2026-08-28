7da8c29ea3e48b9bdbcd5fb5559e9c4b9d8cda67
Aman Chauhan <caman860@gmail.com>
2026-08-29 00:50:39 +0530

    style: replace proprietary __ll_note with standard cout in C++ solutions

--- tmp.cpp
+++ tmp.cpp
@@ -1,16 +1,26 @@
 
 #include "LogicLens.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
 
 // --- INJECTED GLOBAL INPUTS ---
 
-std::string s = "anagram";
-std::string t = "nagaram";
+std::vector<int> nums = {-1, 0, 3, 5, 9, 12};
+int target = 9;
 
 
 struct __LL_Init {
     __LL_Init() {
-        __ll_set_var("s", s);
-        __ll_set_var("t", t);
+        __ll_set_var("nums", nums);
+        __ll_set_var("target", target);
 
     }
 } __ll_init_instance;
@@ -18,29 +28,27 @@ struct __LL_Init {
 // ------------------------------
 
 // --- USER CODE ---
-if (s.length() != t.length()) {
-  __ll_note("Lengths differ, not an anagram");
-  return false;
-}
+int main() {
+// Binary Search C++ â€” O(log n)
+int left = 0;
+int right = nums.size() - 1;
 
-for (int i = 0; i < s.length(); i++) {
-  for (int j = i + 1; j < s.length(); j++) {
-    if (s[i] > s[j]) { char temp = s[i]; s[i] = s[j]; s[j] = temp; }
+while (left <= right) {
+  int mid = left + (right - left) / 2;
+  int midVal = nums[mid];
+  
+  if (midVal == target) {
+    __ll_note("Found target at index " + __ll_to_json(mid));
+    return 0;
+  } else if (midVal < target) {
+    __ll_note("midVal < target, search right half");
+    left = mid + 1;
+  } else {
+    __ll_note("midVal > target, search left half");
+    right = mid - 1;
   }
 }
-for (int i = 0; i < t.length(); i++) {
-  for (int j = i + 1; j < t.length(); j++) {
-    if (t[i] > t[j]) { char temp = t[i]; t[i] = t[j]; t[j] = temp; }
-  }
+return 0;
+return 0;
 }
-
-for (int i = 0; i < s.length(); i++) {
-  if (s[i] != t[i]) {
-    __ll_note("Mismatch found");
-    return false;
-  }
-}
-
-__ll_note("Valid anagram!");
-return true;
 // -----------------
